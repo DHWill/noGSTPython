780081fea657bc97b9d3f42896e8a2deb5adfaa6
williamparry1 <will@dominicharris.studio>
2021-04-14 16:33:29 +0100

    added pipeline Testing

--- testing.py
+++ testing.py
@@ -21,17 +21,16 @@ def main():
     
     
     while 1:
-        t = time.time()
         seg, addr = s.recvfrom(MAX_DGRAM)
         dataIn = np.frombuffer(seg, dtype=np.uint8)
         _image = (len(dataIn) - ((width * height)  * 3))
         header, image = dataIn[:_image], dataIn[_image:]
+        print("TWOT", seg)
 
         image = np.frombuffer(image, dtype=np.uint8)
         image = image.reshape(([height, width, 3]))
         cv2.imshow("image",image)
-        _total = time.time() - t
-        #print(header)
+        print(header)
         #print(60/_total)
 
         if cv2.waitKey(1) & 0xFF == 27:
